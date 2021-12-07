2689b150fa1cbac7b7599379a0dd5ce38d381779
DeViktor <contato.4years@gmail.com>
2021-12-06 23:01:05 -0400

    commit2

--- script.py
+++ script.py
@@ -7,13 +7,22 @@ folder = r'C:\Users\DeViktor\Documents\resources'
 
 def renameFile(file):
     fileName, fileExtension = os.path.splitext(file)
+    shortFileName = fileName.split('-')
+    if shortFileName[0] == "gloves":
+        return f'{fileName.replace(fileName, "Gloves-Dark")}{fileExtension}'
+    if shortFileName[0] == "cloth":
+        return f'{fileName.replace(fileName, "Cloth-Dark")}{fileExtension}'
+    if shortFileName[0] == "skin":
+        return f'{fileName.replace(fileName, "Skin-Dark")}{fileExtension}'
+
     return fileName, fileExtension
 
 
 def fileLoop(root, dirs, files):
     for file in files:
-        rename = renameFile(file)
-        print(rename)
+        renamed = renameFile(file)
+        # print(renamed[0],"\n")
+        print(renamed)
 
 
 def loop():
